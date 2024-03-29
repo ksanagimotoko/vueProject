12571917906424f88272006665fb3803b8ee5125
kimhayoon <>
2024-03-29 09:02:29 +0900

    Vue2  - Add layouts and tips

--- src/BMI/BMICalculator.vue
+++ src/BMI/BMICalculator.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="app">
     <div class="container">
+
+      <div class="row mt-3 tip">
+        <div class="col-md-12 d-flex justify-content-end">
+            <button @click="tiptool" class="btn btn-secondary btn-block "> 팁 보기 </button>
+          </div>
+      </div>
+
       <div class="row justify-content-center">
         <div class="col-md-6">
 
@@ -18,6 +25,7 @@
 
           <button @click="calculateBMI" class="btn btn-primary btn-block"> BMI 계산하기 </button>
           <button @click="reset" class="btn btn-primary btn-block"> 초기화 </button>
+
         </div>
 
     <div class="result row justify-content-center mt-4" v-if="bmi !== null">
@@ -31,9 +39,16 @@
           </div>
         </div>
       </div>
+
+      <div v-if="showTip" class="tip card">
+              <div class="card-body">
+                <h4>{{ showTipTool.title }}</h4>
+                <p>{{ showTipTool.description }}</p>
+              </div>
+            </div>
     </div>
-</div>
   </div>
+</div>
 
 </template>
 
@@ -47,12 +62,23 @@ import obese1Image from '@/assets/bmiImages/ob1.png';
 import obese2Image from '@/assets/bmiImages/ob2.png';
 import obese3Image from '@/assets/bmiImages/ob3.png';
 
+
+
 export default {
   data() {
     return {
       height: null,
       weight: null,
       bmi: null,
+      showTip: false,
+      tips: [
+        { title: 'Tip 1', description: '하루 세번 규칙적인 식사를 해야 한다.' },
+        { title: 'Tip 2', description: '과일과 야채를 많이 먹는다.' },
+        { title: 'Tip 3', description: '고단백 식단을 구성한다.' },
+        { title: 'Tip 4', description: '백색 식품을 머리해야 한다.' },
+        { title: 'Tip 5', description: '음료수 섭취를 줄인다.' }
+      ],
+      showTipToolIndex: 0,
       bmiImages: {
         underweight: underweightImage,
         normal: normalImage,
@@ -70,7 +96,9 @@ export default {
       if (this.bmi < 18.5) return '저체중';
       else if (this.bmi < 24.9) return '정상';
       else if (this.bmi < 29.9) return '과체중';
-      else return '비만';
+      else if (this.bmi < 34.9) return '경도비만';
+      else if (this.bmi < 39.9) return '중도비만';
+      else return '과도비만';
     },
     bmiImage() {
       if (this.bmi === null) return '';
@@ -80,6 +108,10 @@ export default {
       else if (this.bmi < 34.9) return this.bmiImages.obese1;
       else if (this.bmi < 39.9) return this.bmiImages.obese2;
       else return this.bmiImages.obese3;
+    },
+
+    showTipTool() {
+      return this.tips[this.showTipToolIndex];
     }
   },
   methods: {
@@ -95,6 +127,13 @@ export default {
       this.height = null;
       this.weight = null,
       this.bmi = null;
+    },
+
+    tiptool() {
+      this.showTip = !this.showTip;
+      if (this.showTip) {
+        this.showTipToolIndex = (this.showTipToolIndex + 1) % this.tips.length;
+      }
     }
   }
 };
